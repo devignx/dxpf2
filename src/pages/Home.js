5af2da398537a52e70d8e0c6c83528e3ba964246
Devign Experience <devignx@gmail.com>
2023-01-25 22:54:37 +0530

    min3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,35 +2,33 @@ import avatar from '../assets/hp-ai-clr.gif'
 import stik from '../assets/stik.svg'
 import bg from '../assets/bg.webp'
 import fram from '../assets/frame.svg'
+import '../index.css'
 
 
 const Home = () => {
 
     return(
-        <div className='text-white'>
-            <img className='fixed scale-[200%] rotate-90 md:rotate-0 md:scale-[100%] h-screen object-contain bottt w-screen m-auto bg-cover' src={bg} />
+        <div className='text-white snapy'>
+            <img className='fixed select-none pointer-events-none scale-[200%] rotate-90 md:rotate-0 md:scale-[100%] h-screen object-contain bottt w-screen m-auto bg-cover' src={bg} />
 
-           
-            <div className='h-screen relative'>   
+            <div id='hero' className='h-screen relative'>   
                 {/*<img className='m-auto fixed rotate-45 w-[15rem] -top-1/5' src={fram} /> */}
                 <div className='absolute flex flex-col h-full justify-center m-auto w-full -mt-16'>
                     <h1 className="text-center dmser text-[3rem] md:text-[4rem] leading-tight text-white">Hari Prasad</h1>
-                    <p className='text-center rale font-light tracking-wide text-sm md:text-lg text-slate-400 mt-1'>Artist • Designer • Developer</p>
+                    <p className='text-center rale font-light tracking-wide text-sm md:text-lg text-slate-400 mt-3'>Artist • Designer • Developer</p>
                 </div>
                 <div className='absolute bottom-7'>
-                    <img className='m-auto border-white border-4 select-none grayscale hover:filter-none rounded-full w-[8rem] md:w-[7rem] scaleup' src={avatar} />
+                    <a href='#about'><img className='m-auto border-white border-4 select-none grayscale hover:filter-none rounded-full w-[8rem] md:w-[7rem] scaleup' src={avatar} /></a>
                     <img className='m-auto  w-screen h-12 md:h-auto opacity-80' src={stik} />
                 </div>
             </div>
 
-            <div className='w-full mb-8'>
-                <h1 className="text-center text-white mt-[10%] uppercase text-center font-bold text-2xl md">About</h1>
-                <p className='font-light m-auto tracking-wide text-sm text-slate-400 mt-4 w-10/12 md:w-[68%] text-center'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid
+            <div id='about' className='h-screen w-screen mt-[10%] pt-12 '>
+                <h1 className="text-center text-white uppercase text-center font-bold text-2xl md">About</h1>
+                <p className='font-light m-auto rale tracking-wide text-sm text-slate-400 mt-4 w-10/12 md:w-[68%] text-center'>
+                    Lorem ipsum dolor sit amet, consectetur consecteturconsectetur adipiscing elit, sed do eiusmod tempor incid Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incid
                 </p>
             </div>
-            
-
         </div>
     )
 }

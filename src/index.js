596ddd0998874514a2a0c01c9e18c8d574c7f5c8
Devign Experience <devignx@gmail.com>
2023-01-28 23:37:02 +0530

    pwa

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceworker';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,9 +13,4 @@ root.render(
   </React.StrictMode>
 );
 
-if ("serviceWorker" in navigator){
-  window.addEventListener("load", () => {
-  navigator.serviceWorker.register("/sw.js");
-  })
-}
-  
+serviceWorker.register();

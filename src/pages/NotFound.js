67ed7c64d789276d907988c34f61b61cdc5b64cc
Devign Experience <devignx@gmail.com>
2023-01-29 13:07:19 +0530

    cleanup

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,8 +1,8 @@
 const NotFound = () => {
 
     return(
-        <div>
-            <h1>NotFound</h1>
+        <div className="h-screen w-screen flex justify-center items-center">
+            <h1 className=" text-3xl text-white text-center">NotFound :) Hit back button</h1>
         </div>
     )
 }

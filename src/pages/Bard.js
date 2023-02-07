57f5242dc8dc79fb1c7548f7b78117ad5dbe0de0
Devign Experience <devignx@gmail.com>
2023-02-07 20:35:00 +0530

    bard2

--- src/pages/Bard.js
+++ src/pages/Bard.js
@@ -20,16 +20,15 @@ const NotFound = () => {
         <div className="h-screen w-screen flex flex-col justify-between bg-white relative">
             <img className=' w-screen' src={bg} />
             <div className='absolute top-16 m-auto w-full'> 
-                <img className='m-auto h-[3rem] md:h-[4rem]' src={brd} />
+                <img className='m-auto h-[5rem] md:h-[6rem]' src={brd} />
             </div>
             <div className='w-full'>
-                <div onClick={handleClickOpen}  className='text-center cursor-pointer w-full scale-[66%] transition-all duration-500 ease-in-out py-6 border-solid  m-auto  rounded-2xl'>
+                <div onClick={handleClickOpen} className='text-center cursor-pointer w-full scale-[66%] transition-all duration-500 ease-in-out py-6 border-solid  m-auto  rounded-2xl'>
                     <p className='font-bold text-xl text-blue-500'>HINT</p>      
                     <br/>
                     <h1 className='text-black/50'>Try Asking</h1>
                     <p className='font-semibold mt-1 underline shrink-0'>What will be a better society feels like?</p>
                 </div>
-                <br/>
                 <br/>
                 <div className='relative flex items-center mb-12'>
                     <img onClick={handleClickOpen} className='topppp absolute right-[10%] md:right-[28%] w-[3rem] m-auto' src={mic} />
